bb12f097b039c616c53732dec7c8337b9de73c3e
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2023-10-13 21:10:57 +0100

    switched to SvelteKit app

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../styles.css';
+</script>
+
+<nav class="flex items-center py-5">
+	<img src="/starfield_logo.svg" class="h-16 w-32 mx-5 object-contain" alt="Starfield logo" />
+	<ul class="justify-end w-full flex space-x-5 mx-10">
+		<li>Home</li>
+		<li>Docs</li>
+		<li>About</li>
+	</ul>
+</nav>
+<slot />
---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="text-center flex-col">
+	<div class="flex justify-center items-center">
+		<div class="py-10 w-2/3">
+			<h1 class="text-4xl">Welcome to the Starfield API</h1>
+			<p class="text-lg py-2">
+				The community resource for all things Starfield. Access a variety of data from tacross the
+				Settled Systems in easy to consume formats.
+			</p>
+		</div>
+	</div>
+	<img src="/starfield_stripes.png" alt="Starfield logo" />
+</div>
